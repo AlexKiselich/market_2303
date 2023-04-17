35123d2156a64593c3e65b9151939222b29e079f
Alex Kiselich <alexander.kiselich@gmail.com>
2023-04-17 11:59:52 -0600

    Added total_inventory method and started overstocked itmes for market class

--- lib/market.rb
+++ lib/market.rb
@@ -37,13 +37,23 @@ class Market
     vendors.map do |vendor|
       vendor.inventory.each do |keys, values|
         all_items[keys] = Hash.new
+        all_items[keys][:quantity] = values
+        all_items[keys][:vendors] = vendors_that_sell(keys)
+      end
+    end
+    all_items
+  end
+  
+  def overstocked_items
+    overstock = []
+    total_inventory.map do |items|
+      items.each do |keys, values|
+        keys
         require 'pry'; binding.pry
       end
     end
-    all_items.values.each do |keys, values|
-    end
   end
-
-
-
 end
+
+
+
